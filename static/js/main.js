497aac386fe4b0c82cee020da47c691472b942e8
ROZADEPLOY <pokupatel101010@gmail.com>
2025-05-13 19:42:04 +0500

    Add files via upload

--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,18 @@
 function checkAccess() {
-    const tg = window.Telegram.WebApp;
-    fetch("/check_user", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ user_id: tg.initDataUnsafe.user.id })
-    }).then(res => {
-        if (res.ok) return res.json();
-        else throw new Error("Access denied");
-    }).then(data => {
-        if (data.role === "admin") window.location.href = "/admin";
-        else if (data.role === "staff") window.location.href = "/staff";
-        else alert("Нет доступа");
-    }).catch(() => alert("Ошибка доступа"));
+  const user = window.Telegram.WebApp.initDataUnsafe.user;
+  fetch("/api/check_user", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ user_id: user.id })
+  })
+  .then(res => res.json())
+  .then(data => {
+    if (data.role === "admin") {
+      window.location.href = "/admin";
+    } else if (data.role === "user") {
+      window.location.href = "/user";
+    } else {
+      alert("У вас нет административных прав");
+    }
+  });
 }
